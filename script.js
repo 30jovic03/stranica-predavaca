22cfef889fb9e4b161fb9582a51fa495b2b88a0e
Mladen Jovic <30jovic03@gmail.com>
2021-02-16 20:35:36 +0100

    print day name

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let calendar = {
-	'12/02/2021': {
+	'12.02.2021.': {
 		1: {
 			start: '22:30',
 			end: '24:00',
     }
 	},
-  '13/02/2021': {
+  '13.02.2021.': {
     1: {
       start: '07:00',
       end: '08:30',
@@ -15,7 +15,7 @@ let calendar = {
       end: '15:00',
     }
   },
-  '14/02/2021': {
+  '14.02.2021.': {
     1: {
       start: '09:00',
       end: '11:00',
@@ -25,7 +25,7 @@ let calendar = {
       end: '15:30',
     }
   },
-  '15/02/2021': {
+  '15.02.2021.': {
     1: {
       start: '00:00',
       end: '02:00',
@@ -35,7 +35,7 @@ let calendar = {
       end: '05:30',
     }
   },
-  '16/02/2021': {
+  '16.02.2021.': {
     1: {
       start: '00:30',
       end: '02:00',
@@ -45,7 +45,7 @@ let calendar = {
       end: '04:30',
     }
   },
-  '17/02/2021': {
+  '17.02.2021.': {
     1: {
       start: '07:00',
       end: '08:30',
@@ -63,13 +63,15 @@ let calendar = {
       end: '22:30',
     }
   },
-  '18/02/2021': {
+  '18.02.2021.': {
 		1: {
 			start: '12:30',
 			end: '15:00',
     }
 	}
 }
+
+let days = ['ned', 'pon', 'uto', 'sre', 'cet', 'pet', 'sub'];
 
 let time = [];
 for (let i = 0; i < 25; i++) {
@@ -125,15 +127,13 @@ function createDayRow(weekDay) {
   let day = Object.keys(calendar)[weekDay];
   let fields = setFields(day);
 
-  // get day name from date- - - - - - - - - - -
-  // let d = new Date(day);
-  // dayName = d.toString().split(' ')[0];
-  // console.log(dayName);
+  let dayNumber = new Date(day.split('.').reverse().slice(1).join('-')).getDay();
+  let dayName = days[dayNumber];
 
   let dayRow = document.createElement('tr');
   let dayCell = document.createElement('td');
   dayCell.style = 'display: flex; flex-direction: column; align-items: flex-end; width: 60px; padding-right: 12px;';
-  dayCell.innerHTML = `<h3>uto.</h3><p>${day}</p>`;
+  dayCell.innerHTML = `<h3>${dayName}</h3><p>${day}</p>`;
   dayRow.appendChild(dayCell);
   
   for (let i = 0; i < 48; i++) {
